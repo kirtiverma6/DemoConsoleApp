e961738309ee16c65d33c26db939706155f7fefe
kirti <kirtiv4467@gmail.com>
2023-08-11 09:38:58 -0400

    DemoConsoleApp for question 3 and 4

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ExtensionExample;
+
+Console.WriteLine("Hello, World!");
+
+String upperString = "UPPER STRING";
+
+Console.WriteLine("Is "+ upperString+ "  uppaerCase== " + StringExtension.StartsWithUpperCase(upperString));
+
+String lowerString = "lower string";
+
+Console.WriteLine("Is "+ lowerString + " uppaerCase == " + StringExtension.StartsWithUpperCase(lowerString));
